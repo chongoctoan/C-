a7cca1c83e83ee5d663487faa2960a597c7281ea
chongoctoan <chongoctoan@gmail.com>
2022-12-29 20:43:49 +0700

    up

--- ConsoleApp1/Program.cs
+++ ConsoleApp1/Program.cs
@@ -4,9 +4,71 @@ using ConsoleApp1.btvn2;
 using ConsoleApp1.session1;
 using ConsoleApp1.session2;
 using ConsoleApp1.session4;
+using ConsoleApp1.session5;
 public class Program
 {
     static void Main(string[] args) 
+    {
+        Fetch();
+    }
+    static async void Fetch() 
+    {
+        CallApi ca = new CallApi();
+        Product s = await ca.FetchData();
+        Console.WriteLine(s.ToString());
+    }
+    static void Main6(string[] args) 
+    {
+        //ThreadRun();
+        //Thread t1 = new Thread(ThreadRun);
+        //Thread t2 = new Thread(ThreadRunParam);
+        //t2.IsBackground= true;
+        //t1.Start();
+        //t2.Start("Toan");
+        Number num = new Number() { X = 0, Y = 0 };
+        Thread n1 = new Thread(ThreadLock);
+        Thread n2 = new Thread(ThreadLock);
+        n1.Start(num);
+        n2.Start(num);
+    }
+    static void ThreadLock(object o) 
+    {
+        Number n = (Number)o;
+        for(int i = 0;i<20;i++) 
+        {
+            lock (n) 
+            {
+                n.ChangeXY();
+                n.PrintXY();
+            }
+            try 
+            {
+                Thread.Sleep(1000);
+            }catch(Exception e) { }
+        }
+    }
+    static void ThreadRun() 
+    {
+        for(int i = 0; i < 20; i++) 
+        {
+            Console.WriteLine("i=" + i);
+            try 
+            {
+                Thread.Sleep(1000);
+            }catch(Exception e) { }
+        }
+    }static void ThreadRunParam(object msg) 
+    {
+        for(int i = 0; i < 30; i++) 
+        {
+            Console.WriteLine("i=" + i +" Msg:" + msg);
+            try 
+            {
+                Thread.Sleep(1000);
+            }catch(Exception e) { }
+        }
+    }
+    static void Main5(string[] args) 
     {
         StringToVoid stv = new StringToVoid(ShowMessage);
         StringToVoid stv2 = new StringToVoid(DemoDelegate.SayHello);
